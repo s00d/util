dac799efd03daa13d7e7e391904d17a61e85a7d5
crossjs <liwenfu@crossjs.com>
2017-01-14 03:32:48 +0800

    update tests

--- test/unit/index.js
+++ test/unit/index.js
@@ -1,29 +1,21 @@
 import chai from 'chai'
 import sinonChai from 'sinon-chai'
-import { triggerHTMLEvents, triggerMouseEvents, triggerTouchEvents } from './utils'
 import Promise from 'nuo'
 
 localStorage.clear()
 
 chai.use(sinonChai)
 
-global.triggerHTMLEvents = triggerHTMLEvents
-global.triggerMouseEvents = triggerMouseEvents
-global.triggerTouchEvents = triggerTouchEvents
 global.Promise = Promise
 global.assert = chai.assert
 global.expect = chai.expect
 
-// Reset styles
-document.body.style.margin = '0px'
-document.body.style.padding = '0px'
-
 // require all test files (files that ends with .spec.js)
-const testsContext = require.context('./', true, /\.spec\.js$/)
+const testsContext = require.context('./', true, /^\.[/\\]((?!node_modules).)*\.spec\.js$/)
 testsContext.keys().forEach(testsContext)
 
 // require all src files except index.js for coverage.
 // you can also change this to match only the subset of files that
 // you want coverage for.
-const componentsContext = require.context('../../', true, /^\.[/\\]((?!test|index).)*(\.js)$/)
+const componentsContext = require.context('../../', true, /^\.[/\\]((?!test|index|coverage|node_modules).)*(\.js)$/)
 componentsContext.keys().forEach(componentsContext)
